8aa2976661258118a795bba625dd3a1c34f37c04
TaiTrien <v.v.t.trien@gmail.com>
2019-03-25 23:04:21 +0700

    init prj

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeGithubIcon() {
+    document.getElementById('iconGithub').src = "./public/images/github.svg"
+}
+
+function backGithubNormal() {
+    document.getElementById('iconGithub').src = "./public/images/github-logo.svg"
+}
+
+function changeMailIcon() {
+    document.getElementById('iconMail').src = "./public/images/mail-white.svg"
+}
+
+function backMailNormal() {
+    document.getElementById('iconMail').src = "./public/images/mail-black.svg"
+}
